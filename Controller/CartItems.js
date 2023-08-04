c76575004e596b911cf09832917dfec2e834e5f1
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-08-04 16:28:32 +0530

    added cart  handlers

--- Controller/CartItems.js
+++ Controller/CartItems.js
@@ -1,8 +1,8 @@
-const  CartItemsSchema  = require("../Models/CartItems");
+const CartItemsSchema = require("../Models/CartItems");
 const getAllCartItems = async (req, res) => {
-    try {
-        const response =await CartItemsSchema.find({});
-        res.send(response);
+	try {
+		const response = await CartItemsSchema.find({});
+		res.send(response);
 	} catch (error) {
 		console.log(error);
 		re.send(error);
@@ -10,18 +10,46 @@ const getAllCartItems = async (req, res) => {
 };
 
 const AddToCart = async (req, res) => {
-    const obj = req.body;
-    console.log(obj); 
-    try { 
-        const response = await CartItemsSchema.create(obj);
-        console.log(response)
-        res.send(response);
-        
-    } catch (error) {
-        console.log(error);
-        res.send(error);
-    }
-}
+	const obj = req.body;
+	try {
+		const Product = await CartItemsSchema.findOne({
+			ProductId: obj.ProductId,
+		});
 
+		if (!Product) {
+			const response = await CartItemsSchema.create(obj);
 
-module.exports = {getAllCartItems,AddToCart}
+			res.send(response);
+		} else {
+			const response =
+				await CartItemsSchema.findOneAndUpdate(
+					{ ProductId: obj.ProductId },
+					{ quantity: Product.quantity + obj.quantity }
+				);
+			res.send(response);
+		}
+	} catch (error) {
+		console.log(error);
+		res.send(error);
+	}
+};
+
+const RemoveFromCart = async (req, res) => {
+	try {
+		const ProductId = req.params.ProductId;
+		let response = await CartItemsSchema.findByIdAndDelete(
+			ProductId
+		);
+		console.log(response);
+		res.send("removed");
+	} catch (error) {
+		console.log(error);
+		res.send(error);
+	}
+};
+
+module.exports = {
+	getAllCartItems,
+	AddToCart,
+	RemoveFromCart,
+};
--- Routes/CartItems.js
+++ Routes/CartItems.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {AddToCart,getAllCartItems} = require('../Controller/CartItems')
+const {AddToCart,getAllCartItems,RemoveFromCart} = require('../Controller/CartItems')
 
 router.route('/addToCart').post(AddToCart);
 router.route('/getAllCartItems').get(getAllCartItems)
+router.route('/RemoveFromCart/:ProductId').delete(RemoveFromCart);
 
 
 module.exports = router;
